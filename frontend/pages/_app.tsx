e76ffee6801f88f5ca7a011de1ff2f0cbdd0fad1
Andre Luiz <andreluizkanaan222@gamil.com>
2021-03-18 12:14:10 -0300

    Changes

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import GlobalStyle from '../styles/globals';
+import { ToastProvider } from '../context/toast';
+import { UserProvider } from '../context/user';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <>
-      <Component {...pageProps} />
-      <GlobalStyle />
-    </>
+    <ToastProvider>
+      <UserProvider>
+        <Component {...pageProps} />
+        <GlobalStyle />
+      </UserProvider>
+    </ToastProvider>
   )
 }
 
